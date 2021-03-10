30d819c745160c85ae94fc4350b0414f7945ba02
TamIvAnt <65806865+TamIvAnt@users.noreply.github.com>
2021-03-10 20:56:48 +0300

    add replace theme

--- script.js
+++ script.js
@@ -90,36 +90,84 @@ btnMul.addEventListener('click', onBtnMulClick);
 btnDev.addEventListener('click', onBtnDevClick);
 
 
-// let num1 = +prompt('Введите число: ');
-// let numSysSecond = +prompt('Введите систему счисления в которую надо перевести: ');
+function replaceTheme() {
+   let elem = document.querySelector('#btn-theme');
 
+   elem.addEventListener("click", function () { ChangeTheme(); });
 
-// function numberTrans(num, numSys) {
-//     return num.toString(numSys);
-// }
-// function sumNumber(num, numSys) {
-//     let num2 = +prompt('Введите число с которым сложить: ');
+   function ChangeTheme() {
 
-//     return (num + num2).toString(numSys);
-// }
-// function  minNumber(num, numSys) {
-//     let num2 = +prompt('Введите число от которого отнять: ');
+   let themeLight = '<img src="img/icons8-sun-48.png" alt="sun">';
+   let themeDark = '<img src="img/icons8-moon-phase-48.png" alt="moon">';
 
-//     return (num - num2).toString(numSys);
-// }
-// function  mulNumber(num, numSys) {
-//     let num2 = +prompt('Введите число на которое умножить: ');
+   let h1 = document.querySelector('#h1');
+   let h2 = document.querySelector('#h2');
+   let containerFluid = document.querySelector('.container-fluid');
+   let hint = document.querySelector('.hint');
+   let container = document.querySelector('.container');
+   let btn1 = document.querySelector('.btn1');
+   let btn2 = document.querySelector('.btn2');
+   let btn3 = document.querySelector('.btn3');
+   let btn4 = document.querySelector('.btn4');
+   
 
-//     return (num * num2).toString(numSys);
-// }
-// function  devNumber(num, numSys) {
-//     let num2 = +prompt('Введите число на которое делить: ');
+   if (elem.innerHTML == themeLight) {
+      elem.innerHTML = themeDark;
 
-//     return (num / num2).toString(numSys);
-// }
+      h1.style.color = '#eb5e0b';
+      h2.style.color = '#eb5e0b';
 
-// alert(numberTrans(num1, numSysSecond));
-// alert(sumNumber(num1, numSysSecond));
-// alert(minNumber(num1, numSysSecond));
-// alert(mulNumber(num1, numSysSecond));
-// alert(devNumber(num1, numSysSecond));
+      containerFluid.style.backgroundColor = '#a3d2ca'; 
+      containerFluid.style.color = '#5eaaa8';
+
+      hint.style.color = '#5eaaa8';
+
+      container.style.backgroundColor = '#f8f1f1';
+
+      btn1.style.backgroundColor = '#5eaaa8';
+      btn2.style.backgroundColor = '#5eaaa8';
+      btn3.style.backgroundColor = '#5eaaa8';
+      btn4.style.backgroundColor = '#5eaaa8';
+
+   } else if (elem.innerHTML == themeDark) {
+      elem.innerHTML = themeLight;
+
+      h1.style.color = '#fff600';
+      h2.style.color = '#fff600';
+
+      containerFluid.style.backgroundColor = '#26001b';
+      containerFluid.style.color = '#ff005c';
+
+      hint.style.color = '#fff600';
+
+      container.style.backgroundColor = '#810034';
+
+      btn1.style.backgroundColor = '#ff005c';
+      btn2.style.backgroundColor = '#ff005c';
+      btn3.style.backgroundColor = '#ff005c';
+      btn4.style.backgroundColor = '#ff005c';
+
+   } else {
+      elem.innerHTML = themeDark;
+
+      h1.style.color = '#eb5e0b';
+      h2.style.color = '#eb5e0b';
+
+      containerFluid.style.backgroundColor = '#a3d2ca'; 
+      containerFluid.style.color = '#5eaaa8';
+
+      hint.style.color = '#5eaaa8';
+
+      container.style.backgroundColor = '#f8f1f1'; 
+
+      btn1.style.backgroundColor = '#5eaaa8'; 
+      btn2.style.backgroundColor = '#5eaaa8';
+      btn3.style.backgroundColor = '#5eaaa8';
+      btn4.style.backgroundColor = '#5eaaa8';
+
+   }
+}
+
+}
+
+replaceTheme();
